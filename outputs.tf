6c92f09e6ae39b2ebe2c67178f6bfd2fec79659d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:34 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "api_management_policy_fragments" {
-  description = "All api_management_policy_fragment resources"
-  value       = azurerm_api_management_policy_fragment.api_management_policy_fragments
-}
 output "api_management_policy_fragments_api_management_id" {
-  description = "List of api_management_id values across all api_management_policy_fragments"
-  value       = [for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : v.api_management_id]
+  description = "Map of api_management_id values across all api_management_policy_fragments, keyed the same as var.api_management_policy_fragments"
+  value       = { for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : k => v.api_management_id }
 }
 output "api_management_policy_fragments_description" {
-  description = "List of description values across all api_management_policy_fragments"
-  value       = [for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : v.description]
+  description = "Map of description values across all api_management_policy_fragments, keyed the same as var.api_management_policy_fragments"
+  value       = { for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : k => v.description }
 }
 output "api_management_policy_fragments_format" {
-  description = "List of format values across all api_management_policy_fragments"
-  value       = [for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : v.format]
+  description = "Map of format values across all api_management_policy_fragments, keyed the same as var.api_management_policy_fragments"
+  value       = { for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : k => v.format }
 }
 output "api_management_policy_fragments_name" {
-  description = "List of name values across all api_management_policy_fragments"
-  value       = [for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : v.name]
+  description = "Map of name values across all api_management_policy_fragments, keyed the same as var.api_management_policy_fragments"
+  value       = { for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : k => v.name }
 }
 output "api_management_policy_fragments_value" {
-  description = "List of value values across all api_management_policy_fragments"
-  value       = [for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : v.value]
+  description = "Map of value values across all api_management_policy_fragments, keyed the same as var.api_management_policy_fragments"
+  value       = { for k, v in azurerm_api_management_policy_fragment.api_management_policy_fragments : k => v.value }
 }
 
